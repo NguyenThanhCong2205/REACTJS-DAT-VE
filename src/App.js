557dd3241b36d51f15b5a03ff58207fc18fb306d
Nguyen Thanh Cong <thanhcong5591@gmail.com>
2023-01-27 22:51:11 -0800

    REACT-DAT-VE

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import BaiTapBookingTicket from './BaiTapBookingTicket/BaiTapBookingTicket'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <BaiTapBookingTicket/>
+      </div>
+
+    )
+  }
+}
